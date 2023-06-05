72a4b74580ba3f9f0d227411a67f69dc3b5c0a5e
devbispo <felipebisposousa@hotmail.com>
2023-06-05 16:56:00 -0300

    Tela-Store. Resolvendo #4

--- meu-projeto/src/pages/Store.jsx
+++ meu-projeto/src/pages/Store.jsx
@@ -1,33 +1,61 @@
 import React, { useEffect, useState } from "react";
-import '../pages/Store.css'
+import './Store.css';
+import {BsFillCartCheckFill, BsFillCartPlusFill } from 'react-icons/bs'
+import { getItem, setItem } from "../services/LocalStorageFuncs";
+import {Link} from 'react-router-dom';
 
 export const Store = () => {
-    //inserindo API//
-    const [data, setData] = useState ([]);
+
+    const [data, setData ] = useState([]);
+    const [cart, setCart] = useState(getItem('carrinhoYt') || []);
 
     useEffect(() => {
-        const fetchApi = async() =>{
-            const url = 'https://api.mercadolibre.com/sites/MLB/search?q=celular'
-            const response = await fetch(url)
-            const objJson = await response.json() 
+        const fetchApi = async() => {
+            const url = 'https://api.mercadolibre.com/sites/MLB/search?q=celular';
+            const response = await fetch(url);
+            const objJson = await response.json()
             setData(objJson.results)
-    }
-    fetchApi();
+        }
+        fetchApi()
     }, [])
-    /*Api*/
-    return (
-        <div>
-            <h1>Store</h1>
-            <div>
-                {
-                    data.map((e) => (
-                        <div key = {e.id}> 
-                            <h4>{e.title}</h4>
-                            <img src={e.thumbnail} alt=""/>
-                        </div>
-                    ))
-                }
-            </div>
-            </div>
+
+    const handleClick = (obj) => {
+        const element = cart.find((e) => e.id == obj.id)
+        if(element) {
+            const arrFilter = cart.filter((e) => e.id !== obj.id )
+            setCart(arrFilter)
+            setItem('carrinhoYt', arrFilter)
+        } else {
+            setCart([...cart,obj])
+            setItem('carrinhoYt',[...cart,obj])
+        }
+    }
+
+    return(
+    <div>
+        <Link to = '/cart'>Carrinho</Link>
+        <h1>Store</h1>
+        <div className="ProductArea">
+            {
+                data.map((e)=>(
+                    <div className="PA" key={e.id}>
+                        <h4>{e.title}</h4>
+                        <img src={e.thumbnail} alt="" />
+                        <h4>{e.price}</h4>
+                        <button className="btn"  onClick={() => handleClick(e) }
+                        >
+                            {
+                                cart.some((itemCart) => itemCart.id == e.id  ) ? (
+                                    <BsFillCartCheckFill/>
+                                ) : (
+                                    <BsFillCartPlusFill/>
+                                )
+                            }
+                        </button>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
     )
 }
